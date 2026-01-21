752e0ad91041ca2b90e0a60251496a225ed67445
WaiAgPhyo <139035798+WaiAgPhyo@users.noreply.github.com>
2026-01-21 09:41:00 +0630

    fix : application

--- src/main/java/testing/example/demo/TestingApplication.java
+++ src/main/java/testing/example/demo/TestingApplication.java
@@ -2,9 +2,16 @@ package testing.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestingApplication {
+public class TestingApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestingApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
